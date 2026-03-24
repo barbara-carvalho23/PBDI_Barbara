57e2ca29d42eb6175b479a1a18b164c45047a9e2
Barbara Carvalho <barbaracarvalho1@outlook.com>
2026-03-24 18:08:52 -0300

    EX09 1.1 e 1.2 CASE

--- EX09.sql
+++ EX09.sql
@@ -20,3 +20,45 @@ BEGIN
     END IF;
 END;
 $$
+# QUESTÃO 1.1 CASE 
+DO $$
+DECLARE
+    n INT :=  valor_aleatorio_entre(1,100);
+BEGIN 
+     RAISE NOTICE 'Valor gerado: %', n;
+     CASE
+        WHEN  n % 3 = 0  THEN
+        RAISE NOTICE '% é multiplo de 3',n;
+    ELSE 
+        RAISE NOTICE '% não é multiplo de 3',n;
+    END CASE;
+END;
+$$
+# QUESTÃO 1.2
+DO $$
+DECLARE
+    n INT :=  valor_aleatorio_entre(1,100);
+BEGIN
+    RAISE NOTICE 'Valor gerado: %', n;
+    IF n % 3 = 0 or n % 5 = 0 THEN
+        RAISE NOTICE '% é multiplo de 3 e/ou 5',n;
+    ELSE 
+        RAISE NOTICE '% não é multiplo de 3 e/ou 5',n;
+    END IF;
+END;
+$$
+
+# QUESTÃO 1.1 CASE 
+DO $$
+DECLARE
+    n INT :=  valor_aleatorio_entre(1,100);
+BEGIN 
+     RAISE NOTICE 'Valor gerado: %', n;
+     CASE
+        WHEN  n % 3 = 0 or n % 5 = 0 THEN
+        RAISE NOTICE '% é multiplo de 3 e/ou 5',n;
+    ELSE 
+        RAISE NOTICE '% não é multiplo de 3 e/ou 5',n;
+    END CASE;
+END;
+$$
